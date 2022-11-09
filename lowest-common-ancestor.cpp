1137b913c65c4dadcd9d226267c4bb567286414c
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-08 20:58:11 -0800

    added basic structure

--- lowest-common-ancestor.cpp
+++ lowest-common-ancestor.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
+
+int main(int argc, char const* argv[]) {
   return 0;
+}
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 }
